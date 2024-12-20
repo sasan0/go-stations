93303a38d90bae901c663b354d829f69b1fd0480
sasan0 <sasan0tec@gmail.com>
2024-12-20 22:47:08 +0900

    basic station 5

--- handler/router/router.go
+++ handler/router/router.go
@@ -12,7 +12,7 @@ import (
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
-	mux.Handle("/healthz", middleware.Recovery(middleware.Auth(middleware.IdentifyDevice(middleware.Logger(handler.NewHealthzHandler())))))
+	mux.Handle("/healthz", middleware.Recovery(middleware.IdentifyDevice(middleware.Logger(handler.NewHealthzHandler()))))
 	mux.Handle("/todos", middleware.Recovery(middleware.Auth(middleware.IdentifyDevice(middleware.Logger(handler.NewTODOHandler(service.NewTODOService(todoDB)))))))
 	mux.Handle("/do-panic", middleware.Recovery(middleware.Auth(middleware.IdentifyDevice(middleware.Logger(handler.NewPanicHandler())))))
 	return mux
